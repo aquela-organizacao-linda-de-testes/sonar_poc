02bf4fb1adc135500c48cdc06254567f74e89a2c
Filipe Arruda <101719544+filipehba@users.noreply.github.com>
2022-08-01 15:29:36 -0300

    Update some_code.py

--- src/sonar_poc/some_code.py
+++ src/sonar_poc/some_code.py
@@ -6,3 +6,9 @@ def do_another_something(num1, num2):
         return True
     else:
         return False
+    
+def do_another(num1, num2):
+    if num1 == num2:
+        return True
+    else:
+        return False
