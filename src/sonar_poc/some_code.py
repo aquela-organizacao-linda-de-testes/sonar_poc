b447d63ed8b787553fb51b62c496a27c8388b254
Filipe Arruda <filipe.arruda@tembici.com>
2022-08-01 16:48:53 -0300

    some code

--- src/sonar_poc/some_code.py
+++ src/sonar_poc/some_code.py
@@ -14,3 +14,13 @@ def do_another(num1, num2):
         return True
     else:
         return False
+
+
+def do_something_another(num1, num2):
+    if num1 == num2:
+        return False
+
+    if num1 > 0:
+        return True
+    else:
+        return False
